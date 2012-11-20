9efcb6b33a2b35fddfdf70438180022f3e137f9f
Thomas Klemm <github@tklemm.eu>
2012-11-20 08:38:37 +0100

    Add defaults for user history after initialization

--- app/models/user_history.rb
+++ app/models/user_history.rb
@@ -1,18 +1,21 @@
 class UserHistory < ActiveRecord::Base
   belongs_to :user
 
-  # Defaults on initialize
-  def initialize
-    @followers = {}
-    @friends = {}
-    @statuses = {}
+  # Default hashes
+  after_initialize :init
+
+  # Set the default hashes after initialization
+  def init
+    self.followers ||= {} if self.has_attribute? :followers
+    self.friends   ||= {} if self.has_attribute? :friends
+    self.statuses  ||= {} if self.has_attribute? :statuses
   end
 
   ##
   # Serialization
-  # serialize :followers
-  # serialize :friends
-  # serialize :statuses
+  serialize :followers
+  serialize :friends
+  serialize :statuses
 
   attr_accessible :followers, :friends, :statuses
 end
